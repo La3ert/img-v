80f8952de047501302dc0c5e3b17837f7b935691
La3ert <117122903+La3ert@users.noreply.github.com>
2024-02-15 15:28:17 +0200

    Add files via upload

---
+++ ExifReader/ExifReader/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace ExifReader
+{
+    
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMake = new System.Windows.Forms.Label();
+            this.lblModel = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblOrientation = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(10, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 68);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Make";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(10, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 68);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Model";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(10, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 68);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Picture taken on";
+            // 
+            // lblMake
+            // 
+            this.lblMake.Location = new System.Drawing.Point(207, 105);
+            this.lblMake.Name = "lblMake";
+            this.lblMake.Size = new System.Drawing.Size(138, 68);
+            this.lblMake.TabIndex = 6;
+            this.lblMake.Text = "make";
+            // 
+            // lblModel
+            // 
+            this.lblModel.Location = new System.Drawing.Point(207, 173);
+            this.lblModel.Name = "lblModel";
+            this.lblModel.Size = new System.Drawing.Size(138, 68);
+            this.lblModel.TabIndex = 5;
+            this.lblModel.Text = "model";
+            // 
+            // lblDate
+            // 
+            this.lblDate.Location = new System.Drawing.Point(207, 241);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(138, 68);
+            this.lblDate.TabIndex = 4;
+            this.lblDate.Text = "date";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(10, 309);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 68);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Orientation";
+            // 
+            // lblOrientation
+            // 
+            this.lblOrientation.Location = new System.Drawing.Point(207, 309);
+            this.lblOrientation.Name = "lblOrientation";
+            this.lblOrientation.Size = new System.Drawing.Size(138, 68);
+            this.lblOrientation.TabIndex = 11;
+            this.lblOrientation.Text = "orientation";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 0F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 904F));
+            this.tableLayoutPanel1.Controls.Add(this.button3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.80519F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.194805F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1007, 616);
+            this.tableLayoutPanel1.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(8, 10);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(106, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(898, 577);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 586);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 27);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Select Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(106, 586);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 27);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Check EXIF data";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1007, 616);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblOrientation);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblMake);
+            this.Controls.Add(this.lblModel);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Name = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button4;
+
+        private System.Windows.Forms.Button button3;
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        private System.Windows.Forms.TextBox txtBox_ChageOrientation;
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblOrientation;
+
+        private System.Windows.Forms.Label lblModel;
+        private System.Windows.Forms.Label lblDate;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMake;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ ExifReader/ExifReader/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CompactExifLib;
+
+namespace ExifReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String filePath;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog selectImage = new OpenFileDialog()
+            {
+                DefaultExt = "jpg",
+                Filter = "JPEG Files|*.jpg",
+                FilterIndex = 2
+            };
+            
+            if (selectImage.ShowDialog() == DialogResult.OK)
+            {
+                filePath = selectImage.FileName;
+            }
+           
+            
+            Image img = Image.FromFile(selectImage.FileName);
+            pictureBox1.Image = img;
+        }
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            ExifData TestExif;
+            DateTime DateTaken;
+            
+            TestExif = new ExifData(filePath);
+            TestExif.GetTagValue(ExifTag.DateTimeOriginal, out DateTaken);
+            TestExif.GetTagValue(ExifTag.Make, out string make, StrCoding.Utf8);
+            TestExif.GetTagValue(ExifTag.Model, out string model, StrCoding.Utf8);
+            TestExif.GetTagValue(ExifTag.Orientation, out int orientation);
+            MessageBox.Show("Date: " + DateTaken + 
+                                "\nMake: " + make + 
+                                "\nModel: " + model + 
+                                "\nOrientation: " + orientation);
+        }
+    }
+}
